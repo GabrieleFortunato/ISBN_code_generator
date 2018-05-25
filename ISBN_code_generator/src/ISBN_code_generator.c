da19f69cb5a5d25d97a406666c3cf312d4275942
GabrieleFortunato <gabrielefortunato78@libero.it>
2018-05-25 16:39:28 +0200

    Commit del 25 maggio 2018 ore 16,39

--- ISBN_code_generator/src/ISBN_code_generator.c
+++ ISBN_code_generator/src/ISBN_code_generator.c
@@ -72,17 +72,6 @@ static char isbn_ten(char* isbn){
 	return result;
 }
 
-/**
- * Calcola il carattere di controllo di un ISBN
- * @pre isbn_code_standard assume valore 10 o 13
- * @pre isbn_code_str ha lunghezza pari a isbn_code_standard-1
- * @pre isbn_code_str contiene solo caratteri decimali
- * @post il carattere restituito e' un cifra o una X
- * @post il codice completo ISBN supera la verifica
- * @param isbn_code_str
- * @param isbn_code_standard
- * @return
- */
 char isbn_crtl_code_generator(char isbn_code_str[], int isbn_code_standard){
 	assert(isbn_code_standard==LENGTH_TEN||isbn_code_standard==LENGTH_THIRTEEN);
 	assert(strlen(isbn_code_str)==isbn_code_standard-ONE);
